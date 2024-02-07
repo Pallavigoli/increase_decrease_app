89f1121eb276116dc1fdab81a4f6e9f21e977e93
Pallavi Goli <pallavigoli11@gmail.com>
2024-02-08 01:01:30 +0530

    finalcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react"
+import './App.css'
+class App extends React.Component {
+  render(){ 
+    console.log("this is rendering") 
+     return (
+      <div>
+     <h1 className="head">Counter:{this.state.count}</h1>
+     <br/>
+     <br/>
+    <button id="inc" className="b" onClick={()=>this.increase()}>Increase</button>
+    <button id="dec" className="b" onClick={()=>this.decrease()}>Decrease</button>
     </div>
-  );
-}
+  );}
+  constructor()
+  {
+    super()
+    this.state={
+      count : 0
+    }
+  }
+  increase(){
+    if(this.state.count<10)
+      this.setState( { count : this.state.count + 1 } )
+    }
+  decrease(){ 
+    if(this.state.count>0)
+      this.setState({count:this.state.count -1})
+    }
+  componentDidMount()
+  {
+    console.log("this is mount")
+    }
+    componentDidUpdate()
+    {
+      console.log("this is lalaal update")
+    }
+    componentWillUnmount()
+    {
+      console.log("this is unmouny")
+    }
+  }
 
 export default App;
